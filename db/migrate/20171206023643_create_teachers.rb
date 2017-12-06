b70b07dda2c2f3e5db33f352747ee20990fd945b
LeHang0908 <lehanghd96@gmail.com>
2017-12-06 10:18:20 +0700

    new file

---
+++ db/migrate/20171206023643_create_teachers.rb
@@ -0,0 +1,11 @@
+class CreateTeachers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :email
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
